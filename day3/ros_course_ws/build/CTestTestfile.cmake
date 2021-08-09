0d834ebf6774e67b8ae09e6f2da644ed3eee6305
MohammedYasserAlazoni <moh6411@hotmail.com>
2021-08-09 05:03:09 +0200

    script to run everything

---
+++ day3/ros_course_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamedalazoni/ros_course_ws/src
+# Build directory: /home/mohamedalazoni/ros_course_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("testing_pub_sub")
---
+++ day3/ros_course_ws/build/testing_pub_sub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamedalazoni/ros_course_ws/src/testing_pub_sub
+# Build directory: /home/mohamedalazoni/ros_course_ws/build/testing_pub_sub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
